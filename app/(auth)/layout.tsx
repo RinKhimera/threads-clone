8ec98504595c7b0dbe8572ecece2e781eed7de05
Samuel Pokam <119413624+RinKhimera@users.noreply.github.com>
2023-08-25 18:39:46 +0100

    auth implementation

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "../globals.css"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads Application",
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
