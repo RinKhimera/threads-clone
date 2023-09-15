41cd9d095dc336c8374098a7772deb79a004fb2b
RinKhimera <dixiades@gmail.com>
2023-09-15 19:26:33 +0100

    layout impl

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,8 @@
+import Bottombar from "@/components/shared/Bottombar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
+import Topbar from "@/components/shared/Topbar"
+import { ClerkProvider } from "@clerk/nextjs"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "../globals.css"
@@ -15,8 +20,24 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <Topbar />
+
+          <main>
+            <LeftSidebar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image"
-
 export default function Home() {
   return (
-    <main>
-      <h1>Threads</h1>
-    </main>
+    <div>
+      <h1 className="head-text text-left">Home</h1>
+    </div>
   )
 }
