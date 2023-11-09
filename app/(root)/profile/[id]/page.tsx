ce212f33a80c23a15875c23917a4aa90ca29ae35
Samuel Pokam <119413624+RinKhimera@users.noreply.github.com>
2023-11-10 00:09:46 +0100

    search page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ProfileHeader from "@/components/shared/ProfileHeader"
+import ThreadsTab from "@/components/shared/ThreadsTab"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { profileTabs } from "@/constants"
 import { fetchUser } from "@/lib/actions/user.actions"
@@ -40,12 +41,27 @@ const Page = async ({ params }: { params: { id: string } }) => {
 
                 <p className="max-sm:hidden">{}</p>
 
-                {tab.label === "Thread" && (
-                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 text-tiny-medium text-light-2"></p>
+                {tab.label === "Threads" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 text-tiny-medium text-light-2">
+                    {userInfo?.threads?.length}
+                  </p>
                 )}
               </TabsTrigger>
             ))}
           </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              <ThreadsTab
+                currentUserId={user.id}
+                accountId={userInfo.id}
+                accountType="User"
+              />
+            </TabsContent>
+          ))}
         </Tabs>
       </div>
     </section>
